2c95e88988d37c643726cbffec0dd46f8f28c152
Malte Müller <malte@wearefellows.com>
2016-05-16 20:57:25 +0200

    Optimized sample configuration

--- dist/config_sample.php
+++ dist/config_sample.php
@@ -1,6 +1,22 @@
 <?php 
+    
     // full URL M A R K is installed to
     $installpath = 'http://yourserver.com/yourfolder';
+    
+    // user accounts
+	$userinfo = array(
+    	'Foo' => 'PASSWORD'
+    );
+
+
+    // ----------------------------------------------------------------------
+
+    /*
+        
+    Feel free to change the following options if their descriptions
+    make sense to you
+    
+    */
     
     // name of folder images are stored in
     $imgdir = 'imgs';
@@ -16,11 +32,5 @@
     
     // default thumbnail size
     $thumb_width = 400;
-
-    /* ----------------------------- */
-    
-    // user accounts
-	$userinfo = array(
-    	'Foo' => 'PASSWORD'
-    );
 ?>
+
